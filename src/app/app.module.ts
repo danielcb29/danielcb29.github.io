4fd93929121110ff4f90b187e5b453085211fb72
danielcb29 <danielc.web@gmail.com>
2017-06-10 17:30:43 -0500

    dc-page: Navigation with routes config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
+import { ROUTES } from './app.routes'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -15,7 +17,8 @@ import { NavigationComponent } from './navigation/navigation.component';
   ],
   imports: [
     NgbModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: false})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+export const ROUTES: Routes = [
+  // {path: '', component: HomeComponent},
+  // {path: '404', component: NotFoundComponent},
+  // {path: '**', redirectTo: '404'},
+
+];
