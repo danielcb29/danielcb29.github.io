80b913b87b1fc7b5e16a4cde3a85a6dbb0d5df44
danielcb29 <danielc.web@gmail.com>
2017-06-12 20:28:35 -0500

    dc-page: Skills html

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,39 +8,29 @@ import $ from 'jquery/dist/jquery';
 })
 export class SkillsComponent implements OnInit {
 
-  el: any;
-
   constructor() { }
 
   ngOnInit() {
-    $('.skill-progress').bind('inview', (event, visible, visiblePartX, visiblePartY) => {
-        if (visible) {
-            $.each($('div.progress-bar'), () => {
-                $(window).css('width', $(window).attr('aria-valuenow') + '%');
-            });
-            $(window).unbind('inview');
-        }
-    });
 
-    $('.more-skill').bind('inview', (event, visible, visiblePartX, visiblePartY) => {
-        if (visible) {
-            $('.chart').easyPieChart({
-                easing: 'easeOut',
-                delay: 3000,
-                barColor: '#68c3a3',
-                trackColor: 'rgba(255,255,255,0.2)',
-                scaleColor: false,
-                lineWidth: 8,
-                size: 140,
-                animate: 2000,
-                onStep: (from, to, percent) => {
-                    this.el.children[0].innerHTML = Math.round(percent);
-                }
+      $('.more-skill').bind('inview', (event, visible, visiblePartX, visiblePartY) => {
+          if (visible) {
+              $('.chart').easyPieChart({
+                  easing: 'easeOut',
+                  delay: 3000,
+                  barColor: '#68c3a3',
+                  trackColor: 'rgba(255,255,255,0.2)',
+                  scaleColor: false,
+                  lineWidth: 8,
+                  size: 140,
+                  animate: 2000,
+                  onStep: (from, to, percent) => {
+                      $('.skill-progress').el.children[0].innerHTML = Math.round(percent);
+                  }
 
-            });
-            $(window).unbind('inview');
-        }
-    });
+              });
+              $('.skill-progress').unbind('inview');
+          }
+      });
   }
 
 }
