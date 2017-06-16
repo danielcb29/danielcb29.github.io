62355bf79fb74c84d3616bed4ac74c0e74dd85ac
dcorreab <dcorreab@psl.com.co>
2017-06-16 16:12:16 -0500

    dc-page: Skills progress bar

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -27,6 +27,15 @@ export class SkillsComponent implements OnInit {
       }
     };
 
+    devSkills = {
+        frontEnd: '95%',
+        backEnd: '80%',
+        sourceControl: '90%',
+        dataBase: '70%',
+        devOps: '30%',
+        iOT: '40%'
+    }
+
   constructor() { }
 
   ngOnInit() {
