18f71ffc134d72b14e4ad8dc67af3f7f98c1777f
danielcb29 <danielc.web@gmail.com>
2017-06-29 15:02:41 -0500

    Issues #6, #3, #7, #9: Spelling details

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -9,10 +9,10 @@ export class AboutComponent implements OnInit {
 
   information = {
     name: 'Daniel Correa',
-    birth: '29 January 1994',
+    birth: 'January, 29th 1994',
     city: 'Medellín, Colombia',
-    nationality: 'Colombia',
-    phone: '+57 312 769 34 02',
+    nationality: 'Colombian',
+    phone: '(+57) 312 769 34 02',
     email: 'danielc.web@gmail.com'
   }
 
